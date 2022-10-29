d007fc078fb7abb68b2b0bd5190e75177d9bb0c8
GiancarloSanchez-c <gian_23080@hotmail.com>
2022-10-29 18:30:11 -0500

    deleting images

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -97,7 +97,7 @@ export const Banner = () => {
                     isVisible ? "animate__animated animate__zoomIn" : ""
                   }
                 >
-                  <img src={headerImg} alt="Header Img" />
+                 {/*  <img src={headerImg} alt="Header Img" /> */}
                 </div>
               )}
             </TrackVisibility>
